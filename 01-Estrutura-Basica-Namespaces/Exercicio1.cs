417a8a2f60aabaa86859cbb06dc4b5b33fbf60a0
Matheus Westphal <matheuswestphal47@gmail.com>
2026-07-31 11:56:32 -0300

    Adicionar arquivos de projeto.

---
+++ 01-Estrutura-Basica-Namespaces/Exercicio1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estudos
+{
+    internal class Exercicio01
+    { 
+        public static void Executar()
+        {
+            Console.WriteLine("Matheus Westphal dos Santos");
+        }
+    }
+    internal class  Exercicio02
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Apredendo C#");
+            Console.WriteLine("Curso Bóson Treinamentos");
+            Console.WriteLine("Aula 02 - Namespaces e using");
+        }
+    }
+    internal class Exercicio03 
+    {
+        public static void Executar()
+        {
+            Console.Write("Olá, ");
+            Console.Write("Matheus");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Fim do programa");
+        }
+    }
+    internal class Exercicio04
+    {
+        public static void Executar()
+        {
+            String nome = "Matheus";
+            Console.WriteLine($"Olá {nome}!");
+            Console.WriteLine($"Bem-vindo ao curso de C#, {nome}");
+            Console.WriteLine($"O nome {nome} tem {nome.Length} caracteres.");
+        }
+    }
+}
---
+++ 01-Estrutura-Basica-Namespaces/Program.cs
@@ -0,0 +1,18 @@
+﻿using Estudos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_Estrutura_Basica_Namespaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercicio01.Executar();
+            Exercicio02.Executar();
+            Exercicio03.Executar();
+            Exercicio04.Executar();
+        }
+    }
+}
